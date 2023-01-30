db6428d37a018f6b2fee2de8ff9c46eb1a6d5d99
sovaos42 <sovaos42@gmail.com>
2023-01-30 10:33:06 +0700

    pr2

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,6 @@ let app = new Vue({
         product: "Socks",
         image: "./assets/vmSocks-blue-onWhite.jpg",
         altText: "A pair of socks",
-
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks"
     }
 })
