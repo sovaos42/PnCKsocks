620a6349258ddab93556323238d5eacc0d049883
sovaos42 <sovaos42@gmail.com>
2023-01-30 11:08:07 +0700

    pr4

--- scripts/script.js
+++ scripts/script.js
@@ -15,7 +15,7 @@ let app = new Vue({
                 variantId: 2235,
                 variantColor: 'blue'
             }
-        ]
-
+        ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
     }
 })
