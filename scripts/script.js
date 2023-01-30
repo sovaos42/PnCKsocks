afdeb793dd5b5278bd5421b61eececf4e314cca4
sovaos42 <sovaos42@gmail.com>
2023-01-30 10:30:18 +0700

    pr1

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,10 @@
 let app = new Vue({
     el: '#app',
     data: {
+        description: "A pair of warm, fuzzy socks",
         product: "Socks",
         image: "./assets/vmSocks-blue-onWhite.jpg",
-        altText: "A pair of socks"
+        altText: "A pair of socks",
+
     }
 })
