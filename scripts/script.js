5bb3963e24539f95297d08b83fc7fa726729071a
sovaos42 <sovaos42@gmail.com>
2023-01-30 10:49:32 +0700

    4

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,10 @@
 let app = new Vue({
     el: '#app',
     data: {
-        description: "A pair of warm, fuzzy socks",
         product: "Socks",
-        image: "./assets/vmSocks-blue-onWhite.jpg",
+        image: "./assets/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
-        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks"
+        inStock: true,
+        inventory: 100,
     }
 })
